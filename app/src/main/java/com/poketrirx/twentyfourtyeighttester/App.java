deb50939a8b684794be94115ee85d84940a48e34
Nathan Mentley <nathan@Nathans-MacBook-Air.local>
2022-12-07 20:30:58 -0500

    cleanup

--- app/src/main/java/com/poketrirx/twentyfourtyeighttester/App.java
+++ app/src/main/java/com/poketrirx/twentyfourtyeighttester/App.java
@@ -5,11 +5,6 @@ package com.poketrirx.twentyfourtyeighttester;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.Callable;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.Future;
-import java.util.concurrent.TimeUnit;
 import java.util.stream.IntStream;
 
 import com.poketrirx.twentyfourtyeighttester.impl.simulator.GameLogicImpl;
